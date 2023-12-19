ce56e7228cf836eedf93761cb32bc7522ae59e61
Landon Bouma <landonb@users.noreply.github.com>
2023-12-19 02:29:26 -0600

    Update: Let dynamic-versioning manage package __version__

--- src/ansi_escape_room/__init__.py
+++ src/ansi_escape_room/__init__.py
@@ -9,5 +9,4 @@ from .colored import *  # noqa: F401, F403
 from .fore import *  # noqa: F401, F403
 from .style import *  # noqa: F401, F403
 
-__version_info__ = (1, 4, 2)
-__version__ = "{0}.{1}.{2}".format(*__version_info__)
+__version__ = ""
